d73a05eb9ef4ed5d9bbbed008b0497c1f93d31ca
Benjamin Rosas <Benjamin Rosas>
2020-01-15 19:53:42 -0600

    fonts and an image

--- app/tailwind/config.js
+++ app/tailwind/config.js
@@ -2,7 +2,30 @@
 
 module.exports = {
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      // Tailwind defaults:
+      sans: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      serif: 'Georgia, Cambria, Times New Roman, Times, serif',
+      mono: 'Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace',
+
+      // My fonts
+      script: 'bayshore, sans-serif',
+      titls: 'rexlia, sans-serif'
+    }
   },
   variants: {},
   plugins: []
